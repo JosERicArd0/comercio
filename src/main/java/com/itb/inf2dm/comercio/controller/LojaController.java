be33fa7bbbe112054ac78c7bb85f01c0a90e77fc
rm86318 <rm86318@BF-P4L3-M02.fieb.local>
2023-06-13 09:16:42 -0300

    13/06

--- src/main/java/com/itb/inf2dm/comercio/controller/LojaController.java
+++ src/main/java/com/itb/inf2dm/comercio/controller/LojaController.java
@@ -4,17 +4,32 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.itb.inf2dm.comercio.model.Produto;
+
 @Controller
 @RequestMapping("comercio/produtos")
 
 public class LojaController {	
-	
+	list<Produto> listaDeProdutos = new ArrayList<Produto>();
+
 @GetMapping("/listar")
 
 public String listarProdutos() {
-return "produtos";
+
 		
+Produto p1 = new Produto();
+p1.setid(20l);
+p1.setNome("Máquina de lavar Eletrolux 12 l");
+p1.setcodigoBarras("JJJJJJJJJJJJJJJJJJJJ");
+p1.setPreco("3624.10");
+listaDeProdutos.add(p1);
+return "produtos";
+	}
+
+@GetMapping ("/novo-prod")
+public String novoProduto() {
+	return "novo-prod";
+	
 	}
 }
-
 
--- src/main/java/com/itb/inf2dm/comercio/model/Produto.java
+++ src/main/java/com/itb/inf2dm/comercio/model/Produto.java
@@ -3,14 +3,53 @@ package com.itb.inf2dm.comercio.model;
 public class Produto {
 
 	private Long id;
-	private String descricao;
+	private String Descricao;
 	private String nome;
 	private String codigoBarras;
 	private Double preco;
+	
 	// No projeto orientação objetos, falamos sobre o public, 
 	// um modificador de acesso livre para todas as classes e private, 
 	// o mesmo só que restrito para membros da própria classe.
 	//Membros são atributos/métodos
-	
+	//Métodos setter's e getter's : Adicionare Recuperar dados do atribuito "Respectivamente"
+
+public void setid(Long id) {
+	this.id = id;
+}
+public Long getid() {
+	return id;
 	
 }
+public void setDescricao(String Descricao) {
+	this.Descricao = Descricao;
+}
+public String getDescricao() {
+	return Descricao;
+		}
+public void setcodigoBarras(String codigoBarras) {
+	this.codigoBarras = codigoBarras;
+}
+public String getcodigoBarras() {
+	return codigoBarras;
+		}
+public String getNome() {
+	return nome;
+}
+public void setNome(String nome) {
+	this.nome = nome;
+}
+public String getCodigoBarras() {
+	return codigoBarras;
+}
+public void setCodigoBarras(String codigoBarras) {
+	this.codigoBarras = codigoBarras;
+}
+public Double getPreco() {
+	return preco;
+}
+public void setPreco(Double preco) {
+	this.preco = preco;
+}
+
+	}
